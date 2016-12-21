960f920694917be75526bb738888508c06f40742
lipeng <dianxiangan@aliyun.com>
2016-12-21 18:03:21 +0800

    refactor

--- src/main/java/com/lvbby/codema/tool/coder/JavaDelegateCoder.java
+++ src/main/java/com/lvbby/codema/tool/coder/JavaDelegateCoder.java
@@ -1,4 +1,4 @@
-package com.lvbby.codema.tool.coder.source.handler;
+package com.lvbby.codema.tool.coder;
 
 import com.lvbby.codema.coder.TypedCoderHandler;
 import com.lvbby.codema.tool.coder.source.JavaSourceCoderRequest;
@@ -6,10 +6,11 @@ import com.lvbby.codema.tool.coder.source.JavaSourceCoderRequest;
 /**
  * Created by lipeng on 16/12/21.
  */
-public class JavaSourceRepositotyCoder extends TypedCoderHandler<JavaSourceCoderRequest> {
+public class JavaDelegateCoder extends TypedCoderHandler<JavaSourceCoderRequest> {
     @Override
     public void process(JavaSourceCoderRequest request) throws Exception {
         if (request.getTypeDeclaration()==null) return;
+
 
     }
 }
