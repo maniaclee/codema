7463177f6775b1740cb23e297a395ab59f9b5a94
lipeng <dianxiangan@aliyun.com>
2017-01-06 14:50:47 +0800

    delegate service

---
+++ codema-java/src/main/java/com/lvbby/codema/java/app/testcase/$c__name_Repository.java
@@ -0,0 +1,55 @@
+package com.lvbby.codema.java.app.testcase;
+
+import com.github.javaparser.ast.CompilationUnit;
+import com.lvbby.codema.core.render.TemplateEngine;
+import com.lvbby.codema.core.render.TemplateEngineFactory;
+import com.lvbby.codema.java.app.mvn.MavenConfig;
+import com.lvbby.codema.java.template.*;
+import com.lvbby.codema.java.template.entity.JavaClass;
+import com.lvbby.codema.java.tool.JavaLexer;
+import com.lvbby.codema.java.tool.JavaSrcLoader;
+import org.junit.Test;
+
+import static com.lvbby.codema.java.template.$Symbols_.$class_;
+
+/**
+ * Created by lipeng on 2016/12/24.
+ * 朕心甚慰！！！！
+ */
+public class $c__name_Repository {
+
+    private $TemplateClass_ $templateClass_;
+
+    // <%
+    // for( m in c.methods){
+    //var invoke = m.name;
+    //var Class1 = m.returnType;
+    //var class = @m.getArgsInvoke();
+    //var signature = m.argsSignature;
+    // %>
+    @Test
+    public $Class1_ $invoke_($Null_ $signature_) throws Exception {
+        // <% if (@m.isVoid()){ %>
+        $templateClass_.$invoke_($class_);
+        //<%}else{%>
+        $Class1_ re = $templateClass_.$invoke_($class_);
+        return re;
+        //<%}%>
+    }
+    // <% }%>
+
+    public static void main(String[] args) {
+        String template = new JavaSrcTemplateParser().parse($c__name_Repository.class);
+        System.out.println(template);
+        CompilationUnit cu = JavaSrcLoader.getJavaSrcCompilationUnit(MavenConfig.class);
+        JavaClass src = new JavaClassParser().parse(cu);
+        TemplateEngine templateEngine = TemplateEngineFactory.create(template);
+        String result = templateEngine
+                .bind("c", src)
+                .bind("TemplateClass", src.getName())
+                .bind("templateClass", JavaLexer.camel(src.getName()))
+                .bind("Null", "")
+                .render();
+        System.err.println(result);
+    }
+}
