505dc8e11d876a20f44d2f09e0464b6c2f2a0075
lipeng <dianxiangan@aliyun.com>
2017-09-12 21:56:26 +0800

    merge file result

---
+++ codema-core/src/main/java/com/lvbby/codema/core/result/MergeCapableFileResult.java
@@ -0,0 +1,17 @@
+package com.lvbby.codema.core.result;
+
+
+import com.lvbby.codema.core.ResultContext;
+
+import java.io.InputStream;
+
+/**
+ * Created by lipeng on 2017/9/12.
+ */
+public interface MergeCapableFileResult extends FileResult {
+
+    /***
+     *获取merge后的result
+     */
+    String parseMergeResult(InputStream dest, ResultContext resultContext);
+}
